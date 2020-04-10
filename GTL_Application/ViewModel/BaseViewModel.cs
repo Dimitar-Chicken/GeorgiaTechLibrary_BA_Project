e19aa7f46826ce376014b61a2c1680e03167d2a6
Dimitar-Chicken <1067632@ucn.dk>
2020-04-10 18:14:22 +0200

    Adding BaseViewModel to handle IPropertyChangedNotify.

---
+++ GTL_Application/ViewModel/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GTL_Application.ViewModel
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if(changed == null)
+            {
+                return;
+            }
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
