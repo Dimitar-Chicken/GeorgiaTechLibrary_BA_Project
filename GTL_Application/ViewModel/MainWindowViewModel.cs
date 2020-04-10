c55c3a6d3703f439de40f6873d7d723b6eba6670
Dimitar-Chicken <1067632@ucn.dk>
2020-04-10 19:08:05 +0200

    Testing binding.

--- GTL_Application/ViewModel/MainWindowViewModel.cs
+++ GTL_Application/ViewModel/MainWindowViewModel.cs
@@ -6,5 +6,10 @@ namespace GTL_Application.ViewModel
 {
     class MainWindowViewModel : BaseViewModel
     {
+        public string Title { get; set; }
+        public MainWindowViewModel()
+        {
+            Title = "Test1";
+        }
     }
 }
