bc09e028379714f47aee664d18d300b224379b1f
Dimitar-Chicken <1067632@ucn.dk>
2020-04-14 15:14:18 +0200

    Adding LibraryItem class for data holding.

---
+++ GTL_Application/Model/LibraryItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GTL_Application.Model
+{
+    public class LibraryItem
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string SubjectArea { get; set; }
+        public string ItemDescription { get; set; }
+        public string TypeName { get; set; }
+    }
+}
