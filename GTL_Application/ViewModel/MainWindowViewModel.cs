06f0bcac7c16f16c31550fcb558cc55696410e51
Dimitar-Chicken <1067632@ucn.dk>
2020-04-14 15:17:37 +0200

    Adding LibraryItemsList & INotifyPropertyChanged event tracking for it; Adding method to retrieve List of LibraryItems from DB.

--- GTL_Application/ViewModel/MainWindowViewModel.cs
+++ GTL_Application/ViewModel/MainWindowViewModel.cs
@@ -1,15 +1,42 @@
-﻿using System;
+﻿using GTL_Application.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace GTL_Application.ViewModel
 {
     class MainWindowViewModel : BaseViewModel
     {
         public string Title { get; set; }
+
+        List<LibraryItem> libraryItems = new List<LibraryItem>();
+        public List<LibraryItem> LibraryItems
+        {
+            get { return libraryItems; }
+            set { SetProperty(ref libraryItems, value); }
+        }
+
+        private ICommand _getLibraryItemsListCommand;
+        public ICommand GetLibraryItemsListCommand
+        {
+            get
+            {
+                return _getLibraryItemsListCommand ?? (_getLibraryItemsListCommand = new CommandHandler(() => GetLibraryItemsList(), () => true));
+            }
+        }
+        public LibraryItemDataAccess libraryItemDataAccess;
         public MainWindowViewModel()
         {
             Title = "Test1";
         }
+
+        public void GetLibraryItemsList()
+        {
+            libraryItemDataAccess = new LibraryItemDataAccess();
+            LibraryItems = libraryItemDataAccess.GetLibraryItemList();
+        }
+
+
     }
 }
