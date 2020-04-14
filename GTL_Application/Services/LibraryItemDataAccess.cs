396e31a7a3532112c59023fad8f89922d0b7adfa
Dimitar-Chicken <1067632@ucn.dk>
2020-04-14 15:14:48 +0200

    Renaming DatabaseConnetion to LibraryItemDataAccess, adding access to LibraryItem.

---
+++ GTL_Application/Services/LibraryItemDataAccess.cs
@@ -0,0 +1,56 @@
+﻿using GTL_Application.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace GTL_Application
+{
+    public class LibraryItemDataAccess
+    {
+        string connectionString;
+        SqlConnection connection;
+
+        public LibraryItemDataAccess()
+        {
+            connectionString = @"Server=ANOOBIS-DESKTOP\SQL2019; Database=GeorgiaTechLibrary_BA_Project_DB; User Id=sa; Password=1234;";
+
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void OpenConnection()
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+        }
+
+        public List<LibraryItem> GetLibraryItemList()
+        {
+            List<LibraryItem> libraryItems = new List<LibraryItem>();
+
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+
+            string query = "SELECT * FROM LibraryItem";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                LibraryItem libraryItem = new LibraryItem();
+                
+                libraryItem.Title = dataReader["Title"].ToString();
+                libraryItem.Author = dataReader["Author"].ToString();
+                libraryItem.SubjectArea = dataReader["SubjectArea"].ToString();
+                libraryItem.ItemDescription = dataReader["ItemDescription"].ToString();
+                // libraryItem.TypeName = dataReader["LibraryItemType"].ToString();
+
+                libraryItems.Add(libraryItem);
+            }
+
+            return libraryItems;
+        }
+    }
+}
