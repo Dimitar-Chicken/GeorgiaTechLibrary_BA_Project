5272555bb738415568a2073a78412080d0c462d7
Dimitar-Chicken <1067632@ucn.dk>
2020-04-20 20:08:44 +0200

    Moving CommandHandler to Services folder.

--- GTL_Application/Services/CommandHandler.cs
+++ GTL_Application/Services/CommandHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
 
-namespace GTL_Application.ViewModel
+namespace GTL_Application.Services
 {
     class CommandHandler : ICommand
     {
