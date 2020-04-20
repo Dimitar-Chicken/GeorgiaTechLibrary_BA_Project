6aac7beff17c5f25badd95ee9a51ccb0501db200
Dimitar-Chicken <1067632@ucn.dk>
2020-04-20 20:09:20 +0200

    Adding inversion of control.

--- GTL_Application/ViewModel/MainWindowViewModel.cs
+++ GTL_Application/ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GTL_Application.Model;
+using GTL_Application.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +10,7 @@ namespace GTL_Application.ViewModel
     public class MainWindowViewModel : BaseViewModel
     {
         public string Title { get; set; }
-
+        protected readonly LibraryItemDataAccess _libraryItemDataAccess;
         List<LibraryItem> libraryItems = new List<LibraryItem>();
         public List<LibraryItem> LibraryItems
         {
@@ -25,16 +26,25 @@ namespace GTL_Application.ViewModel
                 return _getLibraryItemsListCommand ?? (_getLibraryItemsListCommand = new CommandHandler(() => GetLibraryItemsList(), () => true));
             }
         }
-        public LibraryItemDataAccess libraryItemDataAccess;
         public MainWindowViewModel()
+        {
+            _libraryItemDataAccess = new LibraryItemDataAccess();
+            InitializeAll();
+        }
+        public MainWindowViewModel(LibraryItemDataAccess mockLibraryItemDataAccess)
+        {
+            _libraryItemDataAccess = mockLibraryItemDataAccess;
+            InitializeAll();
+        }
+
+        public void InitializeAll()
         {
             Title = "Test1";
         }
 
         public void GetLibraryItemsList()
         {
-            libraryItemDataAccess = new LibraryItemDataAccess();
-            LibraryItems = libraryItemDataAccess.GetLibraryItemList();
+            LibraryItems = _libraryItemDataAccess.GetLibraryItemList();
         }
 
 
