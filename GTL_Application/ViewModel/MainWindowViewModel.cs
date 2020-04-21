379633b59da3800b168b7f60c1fc0479a4ff300c
Dimitar-Chicken <1067632@ucn.dk>
2020-04-21 18:35:59 +0200

    Chaging privacy modifier of libraryItems to private and renaming '_libraryItems' to reflect change; Changing type of _libraryItems & LibraryItems from List to ObservableCollection; Adding Filtering impelementation.

--- GTL_Application/ViewModel/MainWindowViewModel.cs
+++ GTL_Application/ViewModel/MainWindowViewModel.cs
@@ -2,7 +2,11 @@
 using GTL_Application.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
 using System.Text;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace GTL_Application.ViewModel
@@ -10,22 +14,11 @@ namespace GTL_Application.ViewModel
     public class MainWindowViewModel : BaseViewModel
     {
         public string Title { get; set; }
+        private string _searchText;
         protected readonly LibraryItemDataAccess _libraryItemDataAccess;
-        List<LibraryItem> libraryItems = new List<LibraryItem>();
-        public List<LibraryItem> LibraryItems
-        {
-            get { return libraryItems; }
-            set { SetProperty(ref libraryItems, value); }
-        }
-
+        private ObservableCollection<LibraryItem> _libraryItems;
+        private CollectionViewSource _libraryItemsCollection;
         private ICommand _getLibraryItemsListCommand;
-        public ICommand GetLibraryItemsListCommand
-        {
-            get
-            {
-                return _getLibraryItemsListCommand ?? (_getLibraryItemsListCommand = new CommandHandler(() => GetLibraryItemsList(), () => true));
-            }
-        }
         public MainWindowViewModel()
         {
             _libraryItemDataAccess = new LibraryItemDataAccess();
@@ -40,6 +33,80 @@ namespace GTL_Application.ViewModel
         public void InitializeAll()
         {
             Title = "Test1";
+            _libraryItems = new ObservableCollection<LibraryItem>();
+            GetLibraryItemsList();
+            _libraryItemsCollection = new CollectionViewSource
+            {
+                Source = LibraryItems
+            };
+            _libraryItemsCollection.Filter += LibraryItemsCollection_Filter;
+        }
+        public string SearchText
+        {
+            get
+            {
+                return _searchText;
+            }
+            set
+            {
+                _searchText = value;
+                _libraryItemsCollection.View.Refresh();
+            }
+        }
+
+        public ICollectionView SourceCollection
+        {
+            get
+            {
+                return _libraryItemsCollection.View;
+            }
+        }
+
+        public ObservableCollection<LibraryItem> LibraryItems
+        {
+            get { return _libraryItems; }
+            set { SetProperty(ref _libraryItems, value); }
+        }
+
+        public ICommand GetLibraryItemsListCommand
+        {
+            get
+            {
+                return _getLibraryItemsListCommand ?? (_getLibraryItemsListCommand = new CommandHandler(() => GetLibraryItemsList(), () => true));
+            }
+        }
+
+
+        private void LibraryItemsCollection_Filter(object sender, FilterEventArgs e)
+        {
+            if (string.IsNullOrEmpty(SearchText))
+            {
+                e.Accepted = true;
+                return;
+            }
+
+            LibraryItem libraryItem = e.Item as LibraryItem;
+            
+            // Gather a list of all the properties of the LibraryItem object instance.
+            PropertyInfo[] props = libraryItem.GetType().GetProperties();
+            // Iterate over the individual properties and retrieve the values using the Get methods.
+            foreach (var p in props)
+            {
+                var val = p.GetValue(libraryItem);
+                if (val == null)
+                    return;
+
+                // If the property contains the SearchText string, set the FilterEventArgs Accepted flag to true in order to display it in the Collection.
+                if (val.ToString().ToUpper().Contains(SearchText.ToUpper()))
+                {
+                    e.Accepted = true;
+                    return;
+                }
+                else
+                {
+                    e.Accepted = false;
+                }
+            }
         }
 
         public void GetLibraryItemsList()
