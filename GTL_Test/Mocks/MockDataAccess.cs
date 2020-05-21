522cf8453b0fb72f5657193f434f776244d92ebe
Dimitar-Chicken <1067632@ucn.dk>
2020-05-21 23:02:17 +0200

    Fixing ObservableCollection return bug on both mock methods.

--- GTL_Test/Mocks/MockDataAccess.cs
+++ GTL_Test/Mocks/MockDataAccess.cs
@@ -11,45 +11,61 @@ namespace GTL_Test.Mocks
     [ExcludeFromCodeCoverage]
     public class MockDataAccess : IDataAccess
     {
-        private ObservableCollection<ILibraryItem> _libraryItems;
-        private ObservableCollection<ILibraryItemBorrow> _libraryItemBorrows;
-        public MockDataAccess()
-        {
-            _libraryItems = new ObservableCollection<ILibraryItem>();
-            _libraryItemBorrows = new ObservableCollection<ILibraryItemBorrow>();
-        }
 
         public ObservableCollection<ILibraryItemBorrow> GetLibraryItemBorrowsList()
         {
+            ObservableCollection<ILibraryItemBorrow> libraryItemBorrows = new ObservableCollection<ILibraryItemBorrow>();
+
             LibraryItemBorrow libraryItemBorrow1 = new LibraryItemBorrow
             {
-                PersonName = "John Doe",
-                Title = "Test Title",
-                ISBN = "9783161484100",
+                PersonName = "Fredrik Hans",
+                Title = "Bad Title",
+                ISBN = "9783161484400",
                 Status = "On Loan",
                 BorrowDate = new DateTime(),
                 ReturnDate = new DateTime()
             };
+            libraryItemBorrows.Add(libraryItemBorrow1);
 
-            _libraryItemBorrows.Add(libraryItemBorrow1);
+            LibraryItemBorrow libraryItemBorrow2 = new LibraryItemBorrow
+            {
+                PersonName = "Larry Streisand",
+                Title = "Good Title",
+                ISBN = "9783221484100",
+                Status = "On Loan",
+                BorrowDate = new DateTime(),
+                ReturnDate = new DateTime()
+            };
+            libraryItemBorrows.Add(libraryItemBorrow2);
 
-            return _libraryItemBorrows;
+            return libraryItemBorrows;
         }
 
         public ObservableCollection<ILibraryItem> GetLibraryItemList()
         {
+            ObservableCollection<ILibraryItem> libraryItems = new ObservableCollection<ILibraryItem>();
+
             LibraryItem libraryItem1 = new LibraryItem
             {
-                Title = "Sample Title",
-                Author = "Sample Author",
+                Title = "Book of Truths",
+                Author = "Tanek Soone",
                 SubjectArea = "Sample Subject Area",
                 ItemDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                 TypeName = "Book"
             };
+            libraryItems.Add(libraryItem1);
 
-            _libraryItems.Add(libraryItem1);
+            LibraryItem libraryItem2 = new LibraryItem
+            {
+                Title = "Book of Lies",
+                Author = "John Doe",
+                SubjectArea = "Sample Subject Area",
+                ItemDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                TypeName = "Book"
+            };
+            libraryItems.Add(libraryItem2);
 
-            return _libraryItems;
+            return libraryItems;
         }
     }
 }
