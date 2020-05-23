8883e27b9b4d6a08f7387f92a0b99a90d146bf57
Dimitar-Chicken <1067632@ucn.dk>
2020-05-23 18:39:15 +0200

    Adding ExcludeFromCodeCoverage

--- GTL_Application/App.xaml.cs
+++ GTL_Application/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using GTL_Application.Interfaces;
 using GTL_Application.Services;
+using GTL_Application.View;
 using GTL_Application.ViewModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 
 namespace GTL_Application
@@ -8,6 +10,7 @@ namespace GTL_Application
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    [ExcludeFromCodeCoverage]
     public partial class App : Application
     {
         IDataAccess dataAccess;
