94f830decf5d84c96603b56cc78c65396a3f0284
Dimitar-Chicken <1067632@ucn.dk>
2020-05-23 20:47:48 +0200

    Adding CommandHandlerWithParameters to support commands that pass parameters.

---
+++ GTL_Application/Services/CommandHandlerWithParameters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace GTL_Application.Services
+{
+    class CommandHandlerWithParameters : ICommand
+    {
+        private Action<string> _action;
+        private Func<bool> _canExecute;
+
+        public CommandHandlerWithParameters(Action<string> action, Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute.Invoke();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action(parameter.ToString());
+        }
+    }
+}
