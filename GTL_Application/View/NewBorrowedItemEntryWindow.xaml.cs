9bae18e8ab2e533ccbb39871b05329382ddf7130
Dimitar-Chicken <1067632@ucn.dk>
2020-05-23 18:38:20 +0200

    Adding NewBorrowedItemEntry feature.

---
+++ GTL_Application/View/NewBorrowedItemEntryWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using GTL_Application.Interfaces;
+using GTL_Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GTL_Application.View
+{
+    /// <summary>
+    /// Interaction logic for NewBorrowedItemEntryWindow.xaml
+    /// </summary>
+    public partial class NewBorrowedItemEntryWindow : Window
+    {
+        public NewBorrowedItemEntryWindow(INewBorrowedItemEntryViewModel newBorrowedItemEntryViewModel)
+        {
+            InitializeComponent();
+            DataContext = newBorrowedItemEntryViewModel;
+        }
+    }
+}
---
+++ GTL_Application/ViewModel/NewBorrowedItemEntryViewModel.cs
@@ -0,0 +1,63 @@
+﻿using GTL_Application.Interfaces;
+using GTL_Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace GTL_Application.ViewModel
+{
+    public class NewBorrowedItemEntryViewModel : MainWindowViewModel, INewBorrowedItemEntryViewModel
+    {
+        private IDataAccess _dataAccess;
+        private ICommand _getLibraryItemBorrowsListCommand;
+        private ICommand _createNewLibraryItemBorrowEntryCommand;
+        private ObservableCollection<ILibraryItem> _libraryItemList;
+        public NewBorrowedItemEntryViewModel(IDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+            _libraryItemList = new ObservableCollection<ILibraryItem>();
+            GetLibraryItemBorrowsList();
+        }
+
+        public void GetLibraryItemBorrowsList()
+        {
+            _libraryItemList = _dataAccess.GetLibraryItemList();
+        }
+
+        public ObservableCollection<ILibraryItem> LibraryItemList
+        {
+            get
+            {
+                return _libraryItemList;
+            }
+            set
+            {
+                SetProperty(ref _libraryItemList, value);
+            }
+        }
+
+        public ICommand GetLibraryItemBorrowsListCommand
+        {
+            get
+            {
+                return _getLibraryItemBorrowsListCommand ?? (_getLibraryItemBorrowsListCommand = new CommandHandler(() => GetLibraryItemBorrowsList(), () => true));
+            }
+        }
+
+        public ICommand CreateNewLibraryItemBorrowEntryCommand
+        {
+            get
+            {
+                return _createNewLibraryItemBorrowEntryCommand ?? (_createNewLibraryItemBorrowEntryCommand = new CommandHandler(() => CreateNewLibraryItemBorrowEntry(), () => true));
+            }
+        }
+
+        public void CreateNewLibraryItemBorrowEntry()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
