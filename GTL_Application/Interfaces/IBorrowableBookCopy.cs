6da6c30e55a392790808a2451376a17759279346
Dimitar-Chicken <1067632@ucn.dk>
2020-05-23 19:32:30 +0200

    Adding BorrowableBookCopy implementation.

---
+++ GTL_Application/Interfaces/IBorrowableBookCopy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+
+namespace GTL_Application.Interfaces
+{
+    public interface IBorrowableBookCopy
+    {
+        public SecureString ID { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+    }
+}
