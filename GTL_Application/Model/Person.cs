25fdf621b2144fd80666bfd32bdb4988e1affe23
Dimitar-Chicken <1067632@ucn.dk>
2020-05-26 00:35:56 +0200

    Adding Person Model.

---
+++ GTL_Application/Model/Person.cs
@@ -0,0 +1,20 @@
+﻿using GTL_Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+
+namespace GTL_Application.Model
+{
+    public class Person : IPerson
+    {
+        public SecureString SSN { get; set; }
+        public string PersonName { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime MembershipStartDate { get; set; }
+        public DateTime MembershipEndDate { get; set; }
+    }
+}
