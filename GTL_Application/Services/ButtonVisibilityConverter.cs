ad9a52b6f0e3f48daf2a99ad0c1f92473cbc157a
Dimitar-Chicken <1067632@ucn.dk>
2020-05-26 00:38:09 +0200

    Adding ButtonVisibilityConverter and MultibindConverter.

---
+++ GTL_Application/Services/ButtonVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GTL_Application.Services
+{
+    public class ButtonVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string input = (string)value;
+            if (input.ToLower() == "member")
+            {
+                return "Visible";
+            }
+            else
+            {
+                return "Hidden";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
