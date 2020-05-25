95e6973bc7304e7b1d488080b1f4f36b5c82d2b5
Dimitar-Chicken <1067632@ucn.dk>
2020-05-26 00:38:47 +0200

    Adding PersonMembershipDatesWindow.

---
+++ GTL_Application/View/PersonMembershipDatesWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GTL_Application.View
+{
+    /// <summary>
+    /// Interaction logic for PersonMembershipDatesWindow.xaml
+    /// </summary>
+    public partial class PersonMembershipDatesWindow : Window
+    {
+        public PersonMembershipDatesWindow(object[] dates)
+        {
+            InitializeComponent();
+            
+            startDateTextBlock.Text = ((DateTime) dates[0]).ToString();
+            endDateTextBlock.Text = ((DateTime) dates[1]).ToString();
+        }
+    }
+}
