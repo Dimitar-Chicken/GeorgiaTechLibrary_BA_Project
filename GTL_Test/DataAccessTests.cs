19ddd164a892daaf7f3cfca0d4503e15a66ec56c
Dimitar-Chicken <1067632@ucn.dk>
2020-05-26 03:58:06 +0200

    Adding SecureString tests.

---
+++ GTL_Test/DataAccessTests.cs
@@ -0,0 +1,38 @@
+﻿using GTL_Application.Interfaces;
+using GTL_Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Security;
+using System.Text;
+using Xunit;
+
+namespace GTL_Test
+{
+    [ExcludeFromCodeCoverage]
+    public class DataAccessTests
+    {
+        readonly DataAccess dataAccess;
+        public DataAccessTests()
+        {
+            dataAccess = new DataAccess();
+        }
+
+        [Theory]
+        [InlineData("test")]
+        [InlineData("222")]
+        [InlineData("---")]
+        public void TC009_DataAccess_ConvertToSecureString_Passes(string input)
+        {
+            Assert.IsType<SecureString>(dataAccess.ConvertToSecureString(input));
+        }
+
+        [Fact]
+        public void TC010_DataAccess_ConvertSecureStringToString_Passes()
+        {
+            SecureString input = new NetworkCredential("", "51").SecurePassword;
+            Assert.IsType<string>(DataAccess.ConvertSecureStringToString(input));
+        }
+    }
+}
