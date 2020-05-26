6eac82d073a2aa9cf9e4f93baba232edd1e81c32
Dimitar-Chicken <1067632@ucn.dk>
2020-05-26 19:59:44 +0200

    Adding NewBorrowItemEntryViewModelTests.

---
+++ GTL_Test/NewBorrowItemEntryViewModelTests.cs
@@ -0,0 +1,33 @@
+﻿using GTL_Application.Model;
+using GTL_Application.ViewModel;
+using GTL_Test.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Security;
+using System.Text;
+using System.Windows.Controls;
+using Xunit;
+
+namespace GTL_Test
+{
+    [ExcludeFromCodeCoverage]
+    public class NewBorrowItemEntryViewModelTests
+    {
+        readonly MockDataAccess mockDataAccess;
+        readonly NewBorrowedItemEntryViewModel newBorrowedItemEntryViewModel;
+        public NewBorrowItemEntryViewModelTests()
+        {
+            mockDataAccess = new MockDataAccess();
+            newBorrowedItemEntryViewModel = new NewBorrowedItemEntryViewModel(mockDataAccess);
+        }
+
+        [Fact]
+        public void TC007_NewBorrowedItemEntryViewModel_GetBorrowableBookCopiesListCommand_Passes()
+        {
+            newBorrowedItemEntryViewModel.GetBorrowableBookCopiesListCommand.Execute(null);
+            
+            Assert.NotEmpty(newBorrowedItemEntryViewModel.BorrowableBookCopiesList);
+        }
+    }
+}
